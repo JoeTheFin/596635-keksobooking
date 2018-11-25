6385c428404cc97e3f2afc88fede1360949c294e
mishka leonov <joethefina@ya.ru>
2018-11-25 22:22:22 +0300

    сделал предварительно задания 4 и 5

--- js/map.js
+++ js/map.js
@@ -39,30 +39,36 @@ var PHOTOS_ARRAY = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
-function getRandomInt(min, max) {
+var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var divMapPins = document.querySelector('.map__pins');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var mapFiltersContainer = document.querySelector('.map__filters-container');
+
+var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-function createArrayFromRandomParts(array) {
+var createArrayFromRandomParts = function (arr) { //вопрос с этой функцией
   var arrayExample = [];
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     if (Math.random() > 0.5) {
-      arrayExample[i]
+      arrayExample[i];
     } else {
-        arrayExample
+        arrayExample;
       }
   }
   return arrayExample;
 };
 
 var getExampleArray = function (number) {
-  var array = [];
+  var array =[];
   for (var i = 1; i <= number; i++) {
     var locationX = getRandomInt(130, 631);
     var locationY = getRandomInt(130, 631);
-    var object = {
+    array[i] = {
       author: {
-        avatar: 'img/avatars/user0' + (i) + '.png'
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: TITLE_ARRAY[i],
@@ -85,34 +91,68 @@ var getExampleArray = function (number) {
   }
 };
 
-var example = getExampleArray(8);
-console.log(example);
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-var divMapPins = document.querySelector('.map__pins')
-
-var renderMapPin = function () {
+// что это за функция? (клонирование метки и заполнение данных метки)
+var renderMapPin = function (array) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = (object.location.x + 20) + 'px'; //вопрос Денису: как искать атрибут src или style
-  pinElement.style.top = (object.location.y + 40) + 'px';
-  pinElement.src = object.author.avatar;
-  pinElement.alt = object.offer.title;
+
+  pinElement.style.left = (array[i].location.x + pinElement.width / 2) + 'px';
+  pinElement.style.top = (array[i].location.y + pinElement.height) + 'px';
+  pinElement.src = array[i].author.avatar;
+  pinElement.alt = array[i].offer.title;
 
   return pinElement;
 };
 
-var paintMapPin = function () {
-  for (var i = 0; i < number; i++) {
-    var newPinElement = document.createElement('button');
-    divMapPins.appendChild(newPinElement);
+var iterateMapPin = function (array) {
+  var mapFragment = Document.createDocumentFragment();
+
+  for (var j = 0; j < array.length - 1; j++) {
+    var resultMap = renderMapPin(array[i]);
+    mapFragment.appendChild(resultMap);
   }
+
+  divMapPins.appendChild(mapFragment);
 }
 
+var drawDetailInfo = function (array) {
+  var cardElement = cardTemplate.cloneNode(true); // клонировать шаблон в элемент
+  var cardFragment = Document.createDocumentFragment(); // создать фрагмент
 
+  var renderCardElement = function (array) {
+    cardElement.document.querySelector('.popup__title') = array[i].offer.title;
+    cardElement.document.querySelector('.popup__text--address') = array[i].offer.address;
+    cardElement.document.querySelector('.popup__text--price') = array[i].offer.price + '₽/ночь';
+    cardElement.document.querySelector('.popup__type') = array[i].offer.type;
+    if (array[i].offer.type === 'flat') {
+      cardElement.textContent = 'Квартира';
+    } else if (array[i].offer.type === 'bungalo') {
+      cardElement.textContent = 'Бунгало';
+    } else if (array[i].offer.type === 'house') {
+      cardElement.textContent = 'Дом';
+    } else if (array[i].offer.type === 'palace') {
+      cardElement.textContent = 'Дворец';
+    }
+    cardElement.document.querySelector('.popup__text--capacity') = array[i].offer.rooms + 'комнаты для' + array[i].offer.guests + 'гостей';
+    cardElement.document.querySelector('.popup__text--time') = 'Заезд после' + array[i].offer.checkin + ', выезд до ' + array[i].offer.checkout;
+    cardElement.document.querySelector('.popup__features') = array[i].offer.features;
+    cardElement.document.querySelector('.popup__description') = array[i].offer.description;
+    cardElement.document.querySelector('.popup__photos') = array[i].offer.photos; //вывести все фото... это как?
+  };
 
+  for (var l = 0; l < array.length - 1; l++) {
+    var resultCard = renderCardElement(array[i]);
+    cardFragment.appendChild(resultCard);
+  };
 
+  map.remove(cardFragment, mapFiltersContainer);
+  map.insertBefore(cardFragment, mapFiltersContainer);
 
+  // добавляем свойства все в элемент, всё, включая его добавляем во фрагмент
+  // очистить содержимое контейнера
+  // фрагмент вставляем в контейнер
+}
+
+var exampleArray = getExampleArray(8);
+iterateMapPin(exampleArray);
+
+map.classList.remove('map--faded');
